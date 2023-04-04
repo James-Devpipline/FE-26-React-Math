70e41e216bc7b35786abe517a6d4206556b6e96f
james <james@devpipeline.com>
2023-04-03 18:43:54 -0600

    added instructions

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+/* 
+Using an editor of your choice, spin up a react project.
+
+Build a math helper utility that does the following: 
+ - I want a sum function that takes in x and y and returns the sum
+ - I want a sub function that takes in x and y and returns the difference
+ - One function should be exported by default, the other should be a named export
+
+Import both functions into your App.js, and console.log the results.
+*/
+
 function App() {
   return (
     <div className="App">
