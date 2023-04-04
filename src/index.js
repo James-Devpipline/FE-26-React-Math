eb0a35e201c53abb90cf767540b5c473949ad6e1
james <james@devpipeline.com>
2023-04-03 18:52:23 -0600

    spacing to index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-/* 
-Using an editor of your choice, spin up a react project.
-
-Build a math helper utility that does the following: 
- - I want a sum function that takes in x and y and returns the sum
- - I want a sub function that takes in x and y and returns the difference
- - One function should be exported by default, the other should be a named export
-
-Import both functions into your App.js, and console.log the results.
-*/
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
